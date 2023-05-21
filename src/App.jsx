708fbfd8f840b82aa2d6aae0ebdd201a438291c9
hanifrev <hanifrev@gmail.com>
2023-05-21 15:51:20 +0700

    remove import panel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Panel from "./components/Panel";
 import ThePanel from "./components/ThePanel";
 
 function App() {
